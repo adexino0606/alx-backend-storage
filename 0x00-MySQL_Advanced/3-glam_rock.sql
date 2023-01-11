ba518ab6a60be064834d62a7517dc43d116a05ca
adexino0606 <adexino0606@github.com>
2023-01-11 09:20:54 -0800

    try 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Search bands with style Glam rock
--- Durantion current
+-- Lists all bands with Glam rock AS their main style, ranked by their longevity
 
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
